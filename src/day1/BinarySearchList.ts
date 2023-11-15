f035aeab7d0ec3fa3ad3e1d068f0044eb9aa6141
Stephen Hodges <stephen@shodges.io>
2023-11-15 00:58:50 -0500

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2)
+        if (haystack[m] === needle) {
+            return true
+        } else if (haystack[m] > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo < hi);
+
+    return false;
 }
